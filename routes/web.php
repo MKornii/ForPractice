772a617a93bbf7b22357eb85fb2aa75146426e9b
Misha Korniichuk <mkornijchyk@gmail.com>
2023-03-30 18:39:31 +0300

    start admin auth

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
-use App\Http\Controllers\Admin\IndexController;
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/main', [MainController::class, 'index'])->name('main.index');
+Route::get('/welcome', function (){
+    return view ('auth.login');
+});
+
 
 
 
@@ -14,7 +18,7 @@ Route::get('/posts/update', [PostController::class, 'update']);
 Route::get('/posts/delete', [PostController::class, 'delete']);
 
 Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
-    Route::get('dashboard', [IndexController::class, 'dashboard'])->name('admin.dashboard');
+    Route::get('dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
 
     Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
     Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
@@ -26,3 +30,7 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'],
     Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.delete');
     });
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
